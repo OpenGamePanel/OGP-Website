02baf901bf4553cc41442bd8008556e6156ae36d
own3mall <own3mall@gmail.com>
2016-11-17 19:22:39 -0700

    omano's language changes

--- lang/English/modules/dashboard.php
+++ lang/English/modules/dashboard.php
@@ -21,12 +21,12 @@
  * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
  *
  */
-
-define('dashboard_ftp_text', "The FTP client can compress files and send them via email, can uncompress too, edit text files and much more.");
+ 
+define('dashboard_ftp_text', "The FTP client can compress files and send them via email, can uncompress too, edit text files, and much more.");
 define('dashboard_addons_manager_text', "Manage all different types of services with OGP. Choose from over 70 servers supplied directly by OGP.");
 define('dashboard_game_monitor_text', "Watch and monitor your traffic & logs quickly. You can see what servers are up and down.");
 define('dashboard_support_text', "Get support from the OGP Professional Team on the forum. Fast response and great attention.");
-define('welcome_text', "Welcome to the Open Game Panel (OGP). OGP is an open-source software, that allows you to install, start and monitor different game servers.");
+define('welcome_text', "Welcome to the Open Game Panel (OGP). Open Game Panel is an open source project, that allows you to start, monitor and install different game servers.");
 define('any_server_is_online', "Any server is Online");
 define('current_map', "Current Map");
 define('server_ip_port', "IP:Port");
@@ -39,4 +39,5 @@ define('online_servers', "Online Servers");
 define('add_games_in', "If you want to add games you can do it in %s");
 define('currently_online', "Currently Online");
 define('game_servers', "Game Servers");
+define("select_remote_server", "Select remote server");
 ?>
---
+++ lang/French/modules/status.php
@@ -0,0 +1,38 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2016 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define("system_uptime", "Temps de Disponibilité du Serveur");
+define("system_up_time", "Démarré depuis");
+define("system_up_since", "Démarré le");
+define("cpu_usage", "Utilisation du CPU");
+define("cpu_load", "Charge");
+define("ram_usage", "Utilisation de la RAM");
+define("ram_used", "Mémoire Utilisée");
+define("ram_of", "sur");
+define("storage_space", "Espace de Stockage");
+define("storage_total", "Total");
+define("storage_used", "Utilisé");
+define("storage_free", "Libre");
+define("process_monitor", "Moniteur des Processus");
+?>
