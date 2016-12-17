aaa077ac73e82542513089577b00c3d85b5546dc
®omano <zebilaweed@free.fr>
2016-12-17 18:20:52 +0100

    Add cases for ro2, kf1, and kf2 to show their GT images

--- includes/functions.php
+++ includes/functions.php
@@ -87,9 +87,12 @@ function get_map_path($query_name,$mod,$map) {
 	
 	$mod_gt = $mod == "fof" ? "hl2dm" : $mod_gt;
 	$mod_gt = $mod == "insurgency" ? "ins" : $mod_gt;
+	$mod_gt = $mod == "redorchestra2" ? "ro2" : $mod_gt;
+	$mod_gt = $mod == "killingfloor2" ? "kf2" : $mod_gt;
 	$mod_gt = $query_name == "callofduty" ? "cod" : $mod_gt;
 	$mod_gt = $query_name == "callofduty2" ? "cod2" : $mod_gt;
 	$mod_gt = $query_name == "callofduty4" ? "cod4" : $mod_gt;
+	$mod_gt = $query_name == "killingfloor" ? "killingfloor" : $mod_gt;
 
 	$map_paths= array(
 		"http://image.www.gametracker.com/images/maps/160x120/$mod_gt/$map.jpg",
