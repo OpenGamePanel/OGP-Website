26f14a65a6cfda996f56228679253885d97cebcd
ACsiD <drake@popalot.net>
2017-02-27 21:38:30 -0500

    Add genRandomString() to add_user.php

--- modules/user_admin/add_user.php
+++ modules/user_admin/add_user.php
@@ -75,6 +75,14 @@ function exec_ogp_module()
     }
     else
     {
+		function genRandomString($length) {
+			$characters = "0123456789abcdefghijklmnopqrstuvwxyz";
+			$string = "";	
+			for ($p = 0; $p < $length; $p++) {
+				$string .= $characters[mt_rand(0, strlen($characters)-1)];
+			}
+		return $string;
+}
 		$genPassword = genRandomString(8);
 ?>
     <div class="center">
