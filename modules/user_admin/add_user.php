84a40a98b17d94da7392ada3108fb6c811c9273a
ACsiD <drake@popalot.net>
2017-02-27 21:41:39 -0500

    updated add_user.php

--- modules/user_admin/add_user.php
+++ modules/user_admin/add_user.php
@@ -76,13 +76,15 @@ function exec_ogp_module()
     else
     {
 		function genRandomString($length) {
-			$characters = "0123456789abcdefghijklmnopqrstuvwxyz";
-			$string = "";	
-			for ($p = 0; $p < $length; $p++) {
-				$string .= $characters[mt_rand(0, strlen($characters)-1)];
-			}
-		return $string;
+	function genRandomString($length) {
+	$characters = "0123456789abcdefghijklmnopqrstuvwxyz";
+	$string = "";	
+	for ($p = 0; $p < $length; $p++) {
+		$string .= $characters[mt_rand(0, strlen($characters)-1)];
+	}
+	return $string;
 }
+		}
 		$genPassword = genRandomString(8);
 ?>
     <div class="center">
