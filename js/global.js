54319cdbe1ecc0639d0e19d03508f1df8d013c6d
own3mall <own3mall@gmail.com>
2017-05-27 20:34:26 -0600

    Added Show Server ID Functionality

--- js/global.js
+++ js/global.js
@@ -21,11 +21,11 @@ function showHideServerIDShow(linkElem){
 	if($(".serverId:visible").length){
 		$(".serverId").removeClass('hide').addClass('hide');
 		$(linkElem).text($(linkElem).attr('showtext'));
-		$("tr.expand-child td:first").attr('colspan', $("tr.expand-child td:first").attr('colspan') - 1);
+		$("tr.expand-child td:first").attr('colspan', Number($("tr.expand-child td:first").attr('colspan')) - 1);
 	}else{
 		$(".serverId").removeClass('hide');
 		$(linkElem).text($(linkElem).attr('hidetext'));
-		$("tr.expand-child td:first").attr('colspan', $("tr.expand-child td:first").attr('colspan') + 1);
+		$("tr.expand-child td:first").attr('colspan', Number($("tr.expand-child td:first").attr('colspan')) + 1);
 	}
 }
 
