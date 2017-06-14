b3a2434e07c392b51dd2004090ebf7cd186f4020
own3mall <own3mall@gmail.com>
2017-06-14 03:20:36 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Arabic/modules/user_games.php
+++ lang/Arabic/modules/user_games.php
@@ -214,4 +214,6 @@ define('valid_user', "من فضلك حدد مستخدم صالح");
 define('valid_group', "من فضلك حدد مجموعة صالحة");
 define('set_affinity', "Set Server Affinity");
 define('cpu_affinity_info', "Select the CPU core(s) you want to assign to the game server.");
+define('expiration_date_changed', "Expiration date for selected home has been changed.");
+define('expiration_date_could_not_be_changed', "Expiration date for selected home could not be changed.");
 ?>
