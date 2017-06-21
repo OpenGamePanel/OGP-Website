db958d7c907e8f501a6c2aae7ba4481394b5e9e1
rocco27 <rocco@club27.eu>
2017-06-21 23:40:16 +0200

    Fixed Copyrights in the files II.

--- includes/view.php
+++ includes/view.php
@@ -2,7 +2,7 @@
 /*
  *
  * OGP - Open Game Panel
- * Copyright (C) Copyright (C) 2008 - 2014 The OGP Development Team
+ * Copyright (C) 2008 - 2016 The OGP Development Team
  *
  * http://www.opengamepanel.org/
  *
