a1c143ce49ae736656699b2c225bcc3d0739e84e
rocco27 <rocco@club27.eu>
2017-09-09 02:38:41 +0200

    Hotfix for map images in Game Monitor
    - Fixed Call of Duty 4: Modern Warfare
    - Added Call of Duty: World at War
    - Added Call of Duty: Modern Warfare 3

--- includes/functions.php
+++ includes/functions.php
@@ -91,7 +91,9 @@ function get_map_path($query_name,$mod,$map) {
 	$mod_gt = $mod == "killingfloor2" ? "kf2" : $mod_gt;
 	$mod_gt = $query_name == "callofduty" ? "cod" : $mod_gt;
 	$mod_gt = $query_name == "callofduty2" ? "cod2" : $mod_gt;
-	$mod_gt = $query_name == "callofduty4" ? "cod4" : $mod_gt;
+	$mod_gt = $query_name == "callofduty4mw" ? "cod4" : $mod_gt;
+	$mod_gt = $query_name == "callofdutywaw" ? "codww" : $mod_gt;
+	$mod_gt = $query_name == "callofdutymw3" ? "mw3" : $mod_gt;
 	$mod_gt = $query_name == "conanexiles" ? "conan" : $mod_gt;
 
 	$map_paths= array(
