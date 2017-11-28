e3b8dcfceafabb9ddb64fee537a48aee33d8586a
own3mall <own3mall@gmail.com>
2017-11-27 22:19:45 -0700

    Fix for Firefox

--- js/modules/litefm.js
+++ js/modules/litefm.js
@@ -866,6 +866,7 @@ $(document).ready(function(){
 			},
 			/* reset before submitting */
 			beforeSend: function() {
+				refresh = "YES";
 				progress.show();
 				percent.html('0%');
 				percent2.html('0%');
