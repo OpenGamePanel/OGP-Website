70d75662cf4a043b2f373c6ea14432152be38677
own3mall <own3mall@gmail.com>
2018-05-26 16:20:32 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Greek(Greece)/modules/config_games.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2018 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('OGP_LANG_resetting_configs', "Resetting all configs");
+define('OGP_LANG_updating_configs', "Updating old configs");
+define('OGP_LANG_configs_updated_ok', "Configurations reread successfully.");
+define('OGP_LANG_reset_old_configs', "Reset old configs");
+define('OGP_LANG_update_configs', "Update Configs");
+define('OGP_LANG_game_config_setup', "Setup Game Configs");
+define('OGP_LANG_config_reset_warning', "When you clear old configs there might be issues with the assigned game servers, because the identification numbers might not match with the new configurations.");
+define('OGP_LANG_modify_configs_info', "If you want to modify the game configuration, you can modify the files located in %s.");
+define('OGP_LANG_updating_config_from_file', "Updating config from %s file.");
+define('OGP_LANG_error_when_handling_file', "Error occurred when handling file %s.");
+define('OGP_LANG_error_while_adding_cfg_to_db', "Error occurred when adding data from %s to database.");
+define('OGP_LANG_no_configs_found', "No configuration found from %s file.");
+define('OGP_LANG_select_game', "Select Game");
+define('OGP_LANG_create_xml_configs', "Create XML Configs");
+define('OGP_LANG_failed_to_delete_config_from_db', "Failed to delete %s config from database");
+define('OGP_LANG_failed_removing_file', "Falied removing the file %s.");
+define('OGP_LANG_removed_game_cfg_from_disk_and_datbase', "Removed config for %s from storage and database.");
+define('OGP_LANG_delete_game_config_for', "Delete game configuration for %s from disk and database.");
+?>
---
+++ lang/Greek(Greece)/modules/extras.php
@@ -0,0 +1,43 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2018 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+include 'update.php';
+define('OGP_LANG_unable_download', "Unable to download %s.");
+define('OGP_LANG_md5_failed', "MD5 test failed for %s, try again.");
+define('OGP_LANG_uninstall', "Uninstall");
+define('OGP_LANG_install', "Install");
+define('OGP_LANG_uptodate', "Up-to-date");
+define('OGP_LANG_update_available', "Update available");
+define('OGP_LANG_not_installed', "Not installed");
+define('OGP_LANG_installed', "Installed");
+define('OGP_LANG_download_update', "Download/Update");
+define('OGP_LANG_are_you_sure', "Are you sure");
+define('OGP_LANG_uninstalling_module_dataloss', "By uninstalling a module you will lose all data related to it.");
+define('OGP_LANG_remove_files_for', "Would you like to remove all files for");
+define('OGP_LANG_confirm', "Confirm");
+define('OGP_LANG_cancel', "Cancel");
+define('OGP_LANG_extra_themes', "Extra themes");
+define('OGP_LANG_extra_modules', "Extra modules");
+define('OGP_LANG_search_for_updates', "Check for updates");
+?>
