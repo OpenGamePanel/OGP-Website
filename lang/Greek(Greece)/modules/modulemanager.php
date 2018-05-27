4a6dfbc57bf29722a2c46bf4e29b70f0c79f24f2
rocco27 <rocco@club27.eu>
2018-05-27 17:42:07 +0200

    Fixing language files
    - Added missing Greek(Greece) language files

---
+++ lang/Greek(Greece)/modules/modulemanager.php
@@ -0,0 +1,56 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2018 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('OGP_LANG_module_id', "Module ID");
+define('OGP_LANG_module_name', "Module Name");
+define('OGP_LANG_module_folder', "Module Folder");
+define('OGP_LANG_module_version', "Module Version");
+define('OGP_LANG_db_version', "DB Version");
+define('OGP_LANG_modules_available_for_install', "Custom Modules Available for Installation:");
+define('OGP_LANG_install', "Install");
+define('OGP_LANG_uninstall', "Uninstall");
+define('OGP_LANG_modules', "Modules");
+define('OGP_LANG_update_modules', "Update Modules");
+define('OGP_LANG_no_installed_modules', "No installed modules found.");
+define('OGP_LANG_not_complete', "The install/uninstall feature is not complete and might not work properly. WARNING: Data loss may occur in the OGP database!");
+define('OGP_LANG_core_mods_installed', "Installed Core Modules:");
+define('OGP_LANG_custom_mods_installed', "Installed Custom Modules:");
+define('OGP_LANG_installing_module', "Installing module '%s'");
+define('OGP_LANG_successfully_installed_module', "Successfully installed module %s.");
+define('OGP_LANG_failed_to_install_module', "Failed to install module %s.");
+define('OGP_LANG_adding_module', "Adding module called '%s'.");
+define('OGP_LANG_module_already_installed', "Module called '%s' is already installed.");
+define('OGP_LANG_uninstalling_module', "Uninstalling module '%s'");
+define('OGP_LANG_successfully_uninstalled_module', "Successfully uninstalled module %s.");
+define('OGP_LANG_failed_to_uninstall_module', "Failed to uninstall module %s.");
+define('OGP_LANG_module_file_missing', "directory is missing the module.php file.");
+define('OGP_LANG_module_file_missing_info', "is missing the required information.");
+define('OGP_LANG_query_failed', "Failed to execute query");
+define('OGP_LANG_query_failed_2', "to database.");
+define('OGP_LANG_failed_del_db', "Failed to delete module from database.");
+define('OGP_LANG_updated_module', "Updated module: '%s'.");
+define('OGP_LANG_updating_modules', "Updating Modules");
+define('OGP_LANG_updating_finished', "Updating Finished");
+define('OGP_LANG_prereqs_missing', "Prerequisites of %s must be installed on the server before %s can be installed.");
+?>
