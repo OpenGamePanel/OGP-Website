047f5f982d606cf129e14428aa706a7f53bc91e6
own3mall <own3mall@gmail.com>
2018-05-28 02:38:28 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Greek(Greece)/modules/ftp.php
+++ lang/Greek(Greece)/modules/ftp.php
@@ -22,14 +22,14 @@
  *
  */
 
-define('OGP_LANG_ftp_address', "FTP address: %s:%s");
-define('OGP_LANG_change_account_details', "Change account details");
-define('OGP_LANG_remove_account', "Delete account");
-define('OGP_LANG_ftp_account_already_exists', "This FTP account already exists.");
-define('OGP_LANG_full_path', "Home directory");
-define('OGP_LANG_add_ftp_account', "Add FTP account");
-define('OGP_LANG_remote_server', "Remote server");
-define('OGP_LANG_server_name', "Server name");
-define('OGP_LANG_ftp_account_username_too_long', "FTP username is too long. Try a shorter username no longer than 20 characters.");
-define('OGP_LANG_ftp_account_password_too_long', "FTP password is too long. Try a shorter password no longer than 20 characters.");
+define('OGP_LANG_ftp_address', "Διεύθυνση FTP: %s:%s");
+define('OGP_LANG_change_account_details', "Αλλάξτε στοιχεία λογαριασμού");
+define('OGP_LANG_remove_account', "Διαγράψτε τον λογαριασμό");
+define('OGP_LANG_ftp_account_already_exists', "Αυτός ο λογαριασμός FTP υπάρχει ήδη.");
+define('OGP_LANG_full_path', "Αρχικός κατάλογος");
+define('OGP_LANG_add_ftp_account', "Προσθέστε λογαριασμό FTP");
+define('OGP_LANG_remote_server', "Απομακρυσμένος διακομιστής");
+define('OGP_LANG_server_name', "Όνομα διακομιστή");
+define('OGP_LANG_ftp_account_username_too_long', "Το όνομα χρήστη FTP είναι πολύ μεγάλο. Δοκιμάστε ένα συντομότερο όνομα χρήστη που δεν υπερβαίνει τους 20 χαρακτήρες.");
+define('OGP_LANG_ftp_account_password_too_long', "Ο κωδικός πρόσβασης FTP είναι πολύ μεγάλος. Δοκιμάστε έναν συντομότερο κωδικό πρόσβασης που δεν υπερβαίνει τους 20 χαρακτήρες.");
 ?>
