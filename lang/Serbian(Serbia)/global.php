ce60af8b6cabad0cf08bd69576b99f6549a969bc
own3mall <own3mall@gmail.com>
2018-06-10 09:00:17 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

---
+++ lang/Serbian(Serbia)/global.php
@@ -0,0 +1,143 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2018 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('OGP_LANG_lang_charset', "UTF-8");
+define('OGP_LANG_already_logged_in_redirecting_to_dashboard', "You are already logged in, redirecting to Dashboard.");
+define('OGP_LANG_logging_in', "Logging in");
+define('OGP_LANG_redirecting_in', "Redirecting in");
+define('OGP_LANG_refresh_page', "Refresh Page");
+define('OGP_LANG_no_rights', "You do not have rights to access this page.");
+define('OGP_LANG_welcome', "Welcome");
+define('OGP_LANG_logout', "Logout");
+define('OGP_LANG_logout_message', "You are now logged out.");
+define('OGP_LANG_support', "Support");
+define('OGP_LANG_password', "Password");
+define('OGP_LANG_login', "Username");
+define('OGP_LANG_login_button', "Login");
+define('OGP_LANG_solve_captcha', "Human Check");
+define('OGP_LANG_lost_passwd', "Forgot your password?");
+define('OGP_LANG_no_db_connection', "Could not connect to the database.");
+define('OGP_LANG_bad_login', "Invalid username or password.");
+define('OGP_LANG_not_logged_in', "You are not currently logged in.");
+define('OGP_LANG_remove_install', "Please remove install.php file for security reasons.");
+define('OGP_LANG_agent_offline', "The Agent that is controlling this server is offline.");
+define('OGP_LANG_logged_in', "You are logged in as");
+define('OGP_LANG_delete', "Delete");
+define('OGP_LANG_edit', "Edit");
+define('OGP_LANG_actions', "Actions");
+define('OGP_LANG_invalid_subpage', "Invalid subpage.");
+define('OGP_LANG_invalid_home_id', "Invalid home ID entered.");
+define('OGP_LANG_note', "NOTE");
+define('OGP_LANG_hint', "HINT");
+define('OGP_LANG_yes', "Yes");
+define('OGP_LANG_no', "No");
+define('OGP_LANG_on', "On");
+define('OGP_LANG_off', "Off");
+define('OGP_LANG_db_error_invalid_host', "Invalid database host entered.");
+define('OGP_LANG_db_error_invalid_user_and_pass', "Invalid database username and/or password.");
+define('OGP_LANG_db_error_invalid_database', "Invalid database.");
+define('OGP_LANG_db_unknown_error', "Unkown database error: %s");
+define('OGP_LANG_db_error_module_missing', "Required PHP database module missing.");
+define('OGP_LANG_db_error_invalid_db_type', "Invalid database type in config file.");
+define('OGP_LANG_invalid_login_information', "Invalid login information entered.");
+define('OGP_LANG_failed_to_read_config', "Failed to read config file.");
+define('OGP_LANG_account_expired', "Your account has expired.");
+define('OGP_LANG_contact_admin_to_enable_account', "Contact your administrator to re-enable your account.");
+define('OGP_LANG_maintenance_mode_on', "Maintenance mode is on");
+define('OGP_LANG_logging_out_10', "Logging out in 10 seconds");
+define('OGP_LANG_invalid_redirect', "Redirecting");
+define('OGP_LANG_login_title', "Control Panel Login");
+define('OGP_LANG_module_not_installed', "Module is not installed.");
+define('OGP_LANG_no_access_to_home', "You do not have access to this home.");
+define('OGP_LANG_not_available', "N/A");
+define('OGP_LANG_offline', "Offline");
+define('OGP_LANG_online', "Online");
+define('OGP_LANG_invalid_url', "Invalid URL");
+define('OGP_LANG_xml_file_not_valid', "XML file '%s' could not be validated with schema '%s'.");
+define('OGP_LANG_unable_to_load_xml', "Unable to load XML file '%s'. Permission issue?");
+define('OGP_LANG_gamemanager', "Game Manager");
+define('OGP_LANG_game_monitor', "Game Monitor");
+define('OGP_LANG_dashboard', "Dashboard");
+define('OGP_LANG_user_addons', "Addons");
+define('OGP_LANG_ftp', "FTP");
+define('OGP_LANG_shop', "Shop");
+define('OGP_LANG_shop_guest', "Shop");
+define('OGP_LANG_TS3Admin', "TS3 Admin");
+define('OGP_LANG_administration', "Administration");
+define('OGP_LANG_config_games', "Game/Mod Config");
+define('OGP_LANG_modulemanager', "Modules");
+define('OGP_LANG_server', "Servers");
+define('OGP_LANG_settings', "Panel Settings");
+define('OGP_LANG_themes', "Theme Settings");
+define('OGP_LANG_user_admin', "Users");
+define('OGP_LANG_sub_users', "Sub Users");
+define('OGP_LANG_show_groups', "Groups");
+define('OGP_LANG_user_games', "Game Servers");
+define('OGP_LANG_addons_manager', "Addons Manager");
+define('OGP_LANG_ftp_admin', "FTP users");
+define('OGP_LANG_orders', "Orders");
+define('OGP_LANG_services', "Services");
+define('OGP_LANG_shop_settings', "Shop Settings");
+define('OGP_LANG_update', "Update Panel");
+define('OGP_LANG_extras', "Extras");
+define('OGP_LANG_show', "Show");
+define('OGP_LANG_show_all', "Show All Servers");
+define('OGP_LANG_cur_theme', "%s Theme");
+define('OGP_LANG_copyright', "Copyright");
+define('OGP_LANG_all_rights_reserved', "All rights reserved");
+define('OGP_LANG_version', "Version");
+define('OGP_LANG_show_version', "Show Version");
+define('OGP_LANG_queries_executed', "queries executed");
+define('OGP_LANG_lang', "Language");
+define('OGP_LANG_get_size', "Show Size");
+define('OGP_LANG_total_size', "Total size");
+define('OGP_LANG_lgsl', "LGSL");
+define('OGP_LANG_lgsl_admin', "LGSL Admin");
+define('OGP_LANG_rcon', "RCON");
+define('OGP_LANG_watch_logger', "Watch Logger");
+define('OGP_LANG_litefm_settings', "LiteFM Settings");
+define('OGP_LANG_assign_expiration_date', "Assign expiration date");
+define('OGP_LANG_assign_expiration_date_info', "Once it expires the server is unassigned but not removed.");
+define('OGP_LANG_server_expiration_date', "Server expiration date");
+define('OGP_LANG_server_expiration_date_info', "Once it expires the server is removed (database and files).");
+define('OGP_LANG_set_expiration_date', "Set expiration date");
+define('OGP_LANG_admin_dsi', "DSi Settings");
+define('OGP_LANG_user_dsi', "DSi");
+define('OGP_LANG_list_dsi', "DSi List");
+define('OGP_LANG_no_remote_servers', "There is no known remote servers! Add a server to use this function.");
+define('OGP_LANG_no_results_found', "No search results found for %s");
+define('OGP_LANG_tickets', "Support Tickets");
+define('OGP_LANG_news', "News");
+define('OGP_LANG_admin_news', "News Admin");
+define('OGP_LANG_util', "Utilities");
+define('OGP_LANG_fast_download', "Fast Download");
+define('OGP_LANG_fd_user', "Fast Download");
+define('OGP_LANG_cron', "Cron Admin");
+define('OGP_LANG_user_cron', "Cron");
+define('OGP_LANG_faq', "F.A.Q.");
+define('OGP_LANG_mysql_admin', "MySQL Admin");
+define('OGP_LANG_copied', "Copied!");
+define('OGP_LANG_ticket_settings', "Ticket Settings");
+define('OGP_LANG_form', "Register");
+?>
