d77af3ace50bec1e40e2315744ea281d12e5dac7
rocco27 <rocco@club27.eu>
2018-07-21 11:27:36 +0200

    English language updated
    - Refresh a text for a string

--- lang/English/modules/gamemanager.php
+++ lang/English/modules/gamemanager.php
@@ -26,7 +26,7 @@ define('OGP_LANG_game_manager', "Game Manager");
 define('OGP_LANG_no_games_to_monitor', "You do not have any games configured to you that can be monitored.");
 define('OGP_LANG_status', "Status");
 define('OGP_LANG_fail_no_mods', "No mods enabled for this game! You need to ask your OGP admin to add mod(s) for the game assigned for you.");
-define('OGP_LANG_no_game_homes_assigned', "No game servers assigned for you. You need to ask your OGP admin to assign games for you.");
+define('OGP_LANG_no_game_homes_assigned', "You don't have any servers assigned to your account.");
 define('OGP_LANG_select_game_home_to_configure', "Select a game server that you want to configure");
 define('OGP_LANG_file_manager', "File Manager");
 define('OGP_LANG_configure_mods', "Configure mods");
