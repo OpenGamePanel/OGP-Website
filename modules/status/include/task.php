0b1bd6b5e4cedc48f13d26bb60c4ed02bc6692a7
DieFeM <danifmdelloret@hotmail.com>
2018-08-06 00:00:00 +0200

    The task list now uses ps aux
    The previous command was system distribution and language dependant.

--- modules/status/include/task.php
+++ modules/status/include/task.php
@@ -39,7 +39,7 @@ if( isset($_GET['remote_server_id']) && $_GET['remote_server_id'] != "webhost")
 	}else{
 		if($os == "linux"){
 			$taskoutput = array();
-			$taskoutput["task"] = shell_exec ("top -b -c -i -w512 -n2 | awk '/^top/{i++}i==2' | grep 'COMMAND' -A 30");
+			$taskoutput["task"] = shell_exec ("ps aux");
 		}
 	}
 }
