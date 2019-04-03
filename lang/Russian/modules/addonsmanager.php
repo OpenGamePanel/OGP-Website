8cd7e6603d344a19affe79ed3c281f4e50a1281a
own3mall <own3mall@gmail.com>
2019-04-03 04:12:42 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Russian/modules/addonsmanager.php
+++ lang/Russian/modules/addonsmanager.php
@@ -49,7 +49,7 @@ define('OGP_LANG_show_addons_for_selected_game', "Показать все адд
 define('OGP_LANG_linux_games', "Linux Игры:");
 define('OGP_LANG_windows_games', "Windows Игры:");
 define('OGP_LANG_create_addon', "Создать аддон");
-define('OGP_LANG_addons_db', "Addons Database");
+define('OGP_LANG_addons_db', "База данных аддона:");
 define('OGP_LANG_addon_has_been_created', "Аддон %s был создан успешно.");
 define('OGP_LANG_remove_addon', "Удалить аддон");
 define('OGP_LANG_fill_the_url_address_to_a_compressed_file', "Пожалуйста, заполните URL-адрес для сжатых файлов.");
