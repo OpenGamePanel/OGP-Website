0770265ac38e9ea72988c21ecc61f1443732a30f
own3mall <own3mall@gmail.com>
2020-10-01 00:25:54 -0600

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Finnish(Finland)/modules/dashboard.php
+++ lang/Finnish(Finland)/modules/dashboard.php
@@ -22,22 +22,22 @@
  *
  */
 
-define('OGP_LANG_dashboard_ftp_text', "The FTP client can compress files and send them via email, can uncompress too, edit text files and much more.");
+define('OGP_LANG_dashboard_ftp_text', "FTP-asiakas voi pakata tiedostoja ja lähettää ne sähköpostitse, purkaa pakkauksen, muokata tekstitiedostoja ja paljon muuta.");
 define('OGP_LANG_dashboard_addons_manager_text', "Manage all different types of services with OGP. Choose from over 115 servers, which supplied directly by OGP.");
 define('OGP_LANG_dashboard_game_monitor_text', "Watch and monitor your traffic & logs quickly. You can see what servers are up and down.");
 define('OGP_LANG_dashboard_support_text', "Get support from the OGP Professional Team on the forum. Fast response and great attention.");
 define('OGP_LANG_welcome_text', "Welcome to the Open Game Panel (OGP). OGP is an open-source software, that allows you to install, start and monitor different game servers.");
 define('OGP_LANG_any_server_is_online', "Any server is Online");
-define('OGP_LANG_current_map', "Current Map");
-define('OGP_LANG_server_ip_port', "IP:Port");
-define('OGP_LANG_statistics', "Statistics");
-define('OGP_LANG_servers', "Servers");
-define('OGP_LANG_players', "Players");
-define('OGP_LANG_server_status', "Server Status");
+define('OGP_LANG_current_map', "Nykyinen kartta");
+define('OGP_LANG_server_ip_port', "IP:Portti");
+define('OGP_LANG_statistics', "Statistiikka");
+define('OGP_LANG_servers', "Serverit");
+define('OGP_LANG_players', "Pelaajat");
+define('OGP_LANG_server_status', "Serverin tila");
 define('OGP_LANG_no_games_to_monitor', "There are currently no online/offline game servers");
-define('OGP_LANG_online_servers', "Online Servers");
-define('OGP_LANG_add_games_in', "If you want to add games you can do it in %s");
-define('OGP_LANG_currently_online', "Currently Online");
+define('OGP_LANG_online_servers', "Online-palvelimet");
+define('OGP_LANG_add_games_in', "Jos haluat lisätä pelin, voit tehdä sen %s");
+define('OGP_LANG_currently_online', "Tällä hetkellä Online");
 define('OGP_LANG_game_servers', "Game Servers");
 define('OGP_LANG_select_remote_server', "Select Remote Server");
 ?>
