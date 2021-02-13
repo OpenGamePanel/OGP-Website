b0754ea8b6ae2b9cf9c5adafb7093d5d7d509224
own3mall <own3mall@gmail.com>
2021-02-12 23:31:23 -0700

    Transifex Language Updates - Generated by own3mall's auto script.

--- lang/Finnish(Finland)/modules/dashboard.php
+++ lang/Finnish(Finland)/modules/dashboard.php
@@ -23,21 +23,21 @@
  */
 
 define('OGP_LANG_dashboard_ftp_text', "FTP-asiakas voi pakata tiedostoja ja lähettää ne sähköpostitse, purkaa pakkauksen, muokata tekstitiedostoja ja paljon muuta.");
-define('OGP_LANG_dashboard_addons_manager_text', "Manage all different types of services with OGP. Choose from over 115 servers, which supplied directly by OGP.");
-define('OGP_LANG_dashboard_game_monitor_text', "Watch and monitor your traffic & logs quickly. You can see what servers are up and down.");
-define('OGP_LANG_dashboard_support_text', "Get support from the OGP Professional Team on the forum. Fast response and great attention.");
-define('OGP_LANG_welcome_text', "Welcome to the Open Game Panel (OGP). OGP is an open-source software, that allows you to install, start and monitor different game servers.");
-define('OGP_LANG_any_server_is_online', "Any server is Online");
+define('OGP_LANG_dashboard_addons_manager_text', "Hallitse kaikkia erityyppisiä palveluja OGP:lla. Valitse yli 115 palvelimesta, jotka toimivat suoraan OGP:lla.");
+define('OGP_LANG_dashboard_game_monitor_text', "Katso ja tarkkaile liikennettäsi ja lokiasi nopeasti. Näet mitkä palvelimet ovat ylhäällä ja alhaalla.");
+define('OGP_LANG_dashboard_support_text', "Hanki tukea OGP Professional tiimiltä foorumilla. Nopea vastaus ja suuri huomio.");
+define('OGP_LANG_welcome_text', "Tervetuloa Open Game Paneliin (OGP). OGP on avoimen lähdekoodin ohjelmisto, jonka avulla voit asentaa, käynnistää ja valvoa erilaisia pelipalvelimia.");
+define('OGP_LANG_any_server_is_online', "Mikä tahansa palvelin on online-tilassa");
 define('OGP_LANG_current_map', "Nykyinen kartta");
 define('OGP_LANG_server_ip_port', "IP:Portti");
 define('OGP_LANG_statistics', "Statistiikka");
 define('OGP_LANG_servers', "Serverit");
 define('OGP_LANG_players', "Pelaajat");
 define('OGP_LANG_server_status', "Serverin tila");
-define('OGP_LANG_no_games_to_monitor', "There are currently no online/offline game servers");
+define('OGP_LANG_no_games_to_monitor', "Tällä hetkellä ei ole online-/offline-pelipalvelimia");
 define('OGP_LANG_online_servers', "Online-palvelimet");
 define('OGP_LANG_add_games_in', "Jos haluat lisätä pelin, voit tehdä sen %s");
 define('OGP_LANG_currently_online', "Tällä hetkellä Online");
-define('OGP_LANG_game_servers', "Game Servers");
-define('OGP_LANG_select_remote_server', "Select Remote Server");
+define('OGP_LANG_game_servers', "Pelipalvelimet");
+define('OGP_LANG_select_remote_server', "Valitse etäpalvelin");
 ?>
