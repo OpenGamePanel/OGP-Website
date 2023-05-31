f3c020fbc5d3f51ad2c7c4eb46e3ba210a5e4d57
own3mall <own3mall@gmail.com>
2023-05-31 16:54:30 -0600

    Allow Complex Branch Names

--- modules/update/update.php
+++ modules/update/update.php
@@ -47,9 +47,9 @@ function check_file($local_path, $remote_url)
 }
 
 function urlProperEncode($string) {
-    $entities = array('%21', '%2A', '%27', '%28', '%29', '%3B', '%3A', '%40', '%26', '%3D', '%2B', '%24', '%2C', '%2F', '%3F', '%25', '%23', '%5B', '%5D');
-    $replacements = array('!', '*', "'", "(", ")", ";", ":", "@", "&", "=", "+", "$", ",", "/", "?", "%", "#", "[", "]");
-    return str_replace($entities, $replacements, urlencode($string));
+	$entities = array('%21', '%2A', '%27', '%28', '%29', '%3B', '%3A', '%40', '%26', '%3D', '%2B', '%24', '%2C', '%2F', '%3F', '%25', '%23', '%5B', '%5D');
+	$replacements = array('!', '*', "'", "(", ")", ";", ":", "@", "&", "=", "+", "$", ",", "/", "?", "%", "#", "[", "]");
+	return str_replace($entities, $replacements, urlencode($string));
 }
 
 function exec_ogp_module()
