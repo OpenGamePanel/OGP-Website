768259fffcc92585bb9eb72db63f6a770077733e
shine_clown <wunaidexiaochou@gmail.com>
2024-02-24 02:45:04 +0800

    Add chinese language
    Add chinese language

---
+++ lang/zh-cn/install.php
@@ -0,0 +1,69 @@
+<?php
+/*
+ *
+ * OGP - Open Game Panel
+ * Copyright (C) 2008 - 2018 The OGP Development Team
+ *
+ * http://www.opengamepanel.org/
+ *
+ * This program is free software; you can redistribute it and/or
+ * modify it under the terms of the GNU General Public License
+ * as published by the Free Software Foundation; either version 2
+ * of the License, or any later version.
+ *
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU General Public License for more details.
+ *
+ * You should have received a copy of the GNU General Public License
+ * along with this program; if not, write to the Free Software
+ * Foundation, Inc., 59 Temple Place - Suite 330, Boston, MA  02111-1307, USA.
+ *
+ */
+
+define('OGP_LANG_install_lang', "选择您偏好的语言");
+define('OGP_LANG_install_welcome', "欢迎使用 Open Game Panel 安装程序");
+define('OGP_LANG_file_permission_check', "检查所需文件权限");
+define('OGP_LANG_OK', "确定");
+define('OGP_LANG_write_permission_required', "需要写入权限");
+define('OGP_LANG_execute_permission_required', "需要执行权限");
+define('OGP_LANG_create_an_empty_file', "创建一个空文件。");
+define('OGP_LANG_found', "已找到");
+define('OGP_LANG_not_found', "未找到");
+define('OGP_LANG_pear_xxtea_info', "OGP 使用需要 Pear Crypt_XXTEA。在大多数 Linux 发行版中，可以使用以下 Pear 命令安装此模块 'pear install Crypt_XXTEA-beta'。");
+define('OGP_LANG_refresh', "刷新");
+define('OGP_LANG_checking_required_modules', "检查所需模块");
+define('OGP_LANG_checking_optional_modules', "检查可选模块");
+define('OGP_LANG_database_type', "数据库类型");
+define('OGP_LANG_database_settings', "数据库访问设置");
+define('OGP_LANG_database_hostname', "数据库主机名");
+define('OGP_LANG_database_username', "数据库用户名");
+define('OGP_LANG_database_password', "数据库密码");
+define('OGP_LANG_database_name', "数据库名称");
+define('OGP_LANG_database_prefix', "数据库前缀");
+define('OGP_LANG_next', "下一步");
+define('OGP_LANG_encryption_key', "加密密钥（代理）");
+define('OGP_LANG_agent_port', "端口（代理）");
+define('OGP_LANG_unable_to_write_config', "无法写入配置文件。请重新检查写入权限。");
+define('OGP_LANG_admin_login_details', "管理员登录详情");
+define('OGP_LANG_config_written', "配置文件已成功创建。");
+define('OGP_LANG_database_created', "数据库表已成功创建。");
+define('OGP_LANG_admin_login_details_info', "现在我们为您的 Open Game Panel 创建管理员用户。");
+define('OGP_LANG_username', "用户名");
+define('OGP_LANG_repeat_password', "重复密码");
+define('OGP_LANG_email', "电子邮件地址");
+define('OGP_LANG_back', "返回");
+define('OGP_LANG_database_setup_failure', "无法创建数据库。请重新检查您的数据库配置。");
+define('OGP_LANG_php_version_check', "检查 PHP 版本");
+define('OGP_LANG_invalid_username', "您输入了无效的用户名。");
+define('OGP_LANG_password_too_short', "您的密码太短。它必须至少 '%d' 个字符长。");
+define('OGP_LANG_password_contains_invalid_characters', "您的密码包含无效字符。");
+define('OGP_LANG_invalid_email_address', "您输入了无效的电子邮件地址。");
+define('OGP_LANG_setup_complete', "设置已成功完成。Open Game Panel 现在已准备好使用。");
+define('OGP_LANG_remove_install_and_secure_config', "您应该从服务器上删除 install.php 并将您的 includes/config.inc.php 的权限改回 644，以确保安全。");
+define('OGP_LANG_go_to_panel', "点击这里登录您的 OGP。");
+define('OGP_LANG_unable_to_resolve', "如果您无法解决此问题，请访问 OGP 网站 ");
+define('OGP_LANG_slogan', "开源的一员！");
+define('OGP_LANG_default_welcome_title_message', "欢迎！<b style='font-size:12px; font-weight:normal;'>您可以在 '<a href='?m=settings&p=themes'>主题设置</a>' 下的 '<a href='?m=administration&p=main'>管理</a>' 标签中更改此标题。</b>");
+?>
